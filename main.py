9d4fd7a7437825092571f05878fddb0ff63af0d6
Manoj Pandey <manojpandey1996@gmail.com>
2015-10-31 20:03:30 +0530

    modified

--- main.py
+++ main.py
@@ -34,12 +34,19 @@ def experts():
 
 @app.route('/calldata', methods=['GET','POST'])
 def call():
-    client = Exotel('non56','eedaef1428bfc46e254134be41cd5380b85ccc56')
-    from_num = "%011d"% (9560894192) 
-    call_id  = "%011d"% (9243422233)
-    x  = client.call_flow(from_num,call_id,(56743))
-    print x
-    return render_template('data.html')
+	base = "https://non56:eedaef1428bfc46e254134be41cd5380b85ccc56@twilix.exotel.in/v1/Accounts/non56/Calls/connect"
+	from_num = "%011d"% (9560894192) 
+	call_id  = "%011d"% (9243422233)
+	data = {
+            'From': from_num,
+            'CallerId': call_id,
+            'Url': "http://my.exotel.in/exoml/start/56743",
+            'StatusCallback':'http://localhost:5000/calldata'
+    }
+
+	x  = requests.post(base,data)
+	print x
+	return render_template('data.html')
 
 # When developing locally, this will use port 5000, 
 # in production Heroku will set the PORT environment variable.    
